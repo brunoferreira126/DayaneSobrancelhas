3dde5d740f1cf9c1bed30cf010a7809814b7b315
brunoferreira126 <ferreira_brunomendes@hotmail.com>
2024-12-03 15:22:17 -0300

    Primeiro commit: Projeto de Dayane Souza

---
+++ script.js
@@ -0,0 +1,6 @@
+// Placeholder para interações futuras
+document.querySelectorAll(".btn").forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log("Botão clicado:", button.innerText);
+    });
+});
